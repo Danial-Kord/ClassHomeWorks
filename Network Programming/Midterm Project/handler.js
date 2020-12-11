faceedc739c781193f4e32d00f7a1279cd110a8d
Danial <danial.kordmodanlou@gmail.com>
2020-12-11 16:20:09 +0330

    fetching request and handling response js

--- Network Programming/Midterm Project/handler.js
+++ Network Programming/Midterm Project/handler.js
@@ -1,18 +1,39 @@
 var maxfontSize = 30;
 var minfontsize = 24;
 
+function calculateData(data) {
 
-function getData() {
+}
+
+
+function getData(index) {
     window.alert("fetching");
-    fetch("https://swapi.dev/api/people/1/")
+    fetch("https://swapi.dev/api/starships/?page=1")
         .then((resp) => resp.json())
         .then(
-            (data) =>
-            (document.getElementById(
-                "title"
-            ).innerHTML = JSON.stringify(data))
-        );
+            (data) => {
+                i = 1;
+                data = data["results"][0]
+                dadElemnt = document.getElementById("result").children[0];
+
+                let text = ""
+                for (x in data) {
+                    text = x + " : " + data[x];
+                    let newElement = document.createElement("dd");
+                    newElement.innerHTML = text;
+                    dadElemnt.appendChild(newElement);
+                    i++;
+                    if (i == 14) {
+                        break;
+                    }
+                }
+
+                document.getElementById(
+                    "title"
+                ).innerHTML = "hi betch ";
+            })
 }
+
 
 
 let ar = document.getElementsByTagName("td");
@@ -29,9 +50,13 @@ while (i < ar.length) {
     }
     i++;
 }
-i = 0;
-while (i < ar.length) {
-    ar[i].onclick = () => {
+
+
+let br = document.getElementsByTagName("td");
+let h = 0;
+while (h < ar.length) {
+    br[h].onclick = () => {
         getData();
     }
+    h++;
 }
